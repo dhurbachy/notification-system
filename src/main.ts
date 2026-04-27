858e08f20d0da0b3a1104ee32ac18514d881dfca
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-04-27 17:47:34 +0545

    chores: install fastify and configure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 // why this to include swagger in main.ts file
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter() as any,
+  );
   const config = new DocumentBuilder()
     .setTitle('Notification System')
     .setDescription('NestJS API with Fastify and Swagger')
