03970afee5409ffdd7ff08ff0225896cfb96da05
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-04-27 15:03:50 +0545

    chores: configure swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+// why this to include swagger in main.ts file
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Notification System')
+    .setDescription('NestJS API with Fastify and Swagger')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
