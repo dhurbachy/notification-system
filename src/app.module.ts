bd502b3d49cf6cbf008038dad55375e060e0eec6
Dhurba Chaudhary <dhurbac66@gmail.com>
2026-04-29 17:36:39 +0545

    feat: auth resources generated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,16 @@ import { NotificationModule } from './notification/notification.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from './config/typeorm.config';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     (ConfigModule as any).forRoot({ isGlobal: true }) as DynamicModule,
     TypeOrmModule.forRoot(dataSourceOptions),
     NotificationModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
